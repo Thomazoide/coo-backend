7641cc391db82b1e4b1d7b1b95b472855fa2e9dd
Thomas Tellerias <62606308+Thomazoide@users.noreply.github.com>
2023-03-14 11:09:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const connectDB = require('./db')
 const app = require('./app')
+const https = require('https')
 
-async function main(){
-	await app.listen(443)
-	console.log('conectado en el puerto 443')
-}
+// async function main(){
+// 	await app.listen(443)
+// 	console.log('conectado en el puerto 443')
+// }
+
+https
+	.createServer(app)
+	.listen(5555, () => {
+		console.log('Conectado en puerto 5555')
+	}
+
 
 connectDB()
-main()
+
