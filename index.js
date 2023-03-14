19d138f6e4b205431bfc252d0305973ad66c5733
Thomas Tellerias <62606308+Thomazoide@users.noreply.github.com>
2023-03-14 10:56:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const connectDB = require('./db')
 const app = require('./app')
 
 async function main(){
-	await app.listen(5555)
-	console.log('conectado en el puerto 5555')
+	await app.listen(443)
+	console.log('conectado en el puerto 443')
 }
 
 connectDB()
