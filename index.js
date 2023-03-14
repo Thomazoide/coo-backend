c0f4bcfe4e61f2d9e2874624ad5faad23beae5c1
Thomas Tellerias <62606308+Thomazoide@users.noreply.github.com>
2023-03-14 11:55:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const connectDB = require('./db')
 const app = require('./app')
 const https = require('https')
+const fs = require('fs')
 
 // async function main(){
 // 	await app.listen(443)
 // 	console.log('conectado en el puerto 443')
 // }
 
-https
-	.createServer(app)
-	.listen(5555, () => {
-		console.log('Conectado en puerto 5555')
-	})
+const httpsServer = https.createServer(
+	{
+		key: fs.readFileSync(path.join(_dirname, "./", "aws-coo-fm-00.pem")),
+		cert: fs.readFileSync(path.join(_dirname, "./", "cert.pem")),
+	},
+	app
+)
 
+httpsServer.listen(5555, ()=>{console.log('Conectado en puerto 5555')})
 
 connectDB()
 
