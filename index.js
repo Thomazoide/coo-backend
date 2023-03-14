1bece044b46d066852ed235ac2f09bcdae9a3f68
Thomas Tellerias <62606308+Thomazoide@users.noreply.github.com>
2023-03-14 11:57:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const connectDB = require('./db')
 const app = require('./app')
 const https = require('https')
 const fs = require('fs')
+const path = require('path')
 
 // async function main(){
 // 	await app.listen(443)
